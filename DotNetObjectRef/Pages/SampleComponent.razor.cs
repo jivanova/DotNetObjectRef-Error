366b235b8a27488c7c179ee543903336330ad32b
Joana <Joana.Ivanova@telerik.com>
2019-10-11 16:56:45 +0300

    chore: add sample project to test

---
+++ DotNetObjectRef/Pages/SampleComponent.razor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace DotNetObjectRef.Pages
+{
+    public class SampleComponentBase: ComponentBase, IDisposable
+    {
+        private protected readonly string id = Guid.NewGuid().ToString();
+
+        [Inject] 
+        IJSRuntime JsRuntime { get; set; }
+
+        private DotNetObjectReference<SampleComponentBase> Reference { get; set; }
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                if (Reference == null)
+                {
+                    Reference = DotNetObjectReference.Create(this);
+                }
+
+                await JsRuntime.InvokeAsync<object>(
+                        "exampleJsFunctions.SubscribeEvents", id, Reference);
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        public void Dispose()
+        {
+            if (Reference != null)
+            {
+                Reference.Dispose();
+            }
+        }
+
+        [JSInvokable]
+        public void Close()
+        {
+            StateHasChanged();
+        }
+    }
+}
---
+++ DotNetObjectRef/wwwroot/jsinterop.js
@@ -0,0 +1,11 @@
+﻿window.exampleJsFunctions = {
+    SubscribeEvents: function (id, dotNetObjectRef) {
+        const element = document.getElementById(id);
+        debugger;
+        element.addEventListener('click', function (e) {
+            window.setTimeout(function () {
+                dotNetObjectRef.invokeMethodAsync('Close');
+            }, 3000);
+        });
+    },
+};
